6e98769fdba6a46d64763421bedb69f42e772baf
Yu <51410206@qq.com>
2017-04-25 10:08:19 +0800

    update

--- index.android.js
+++ index.android.js
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import {AppRegistry} from 'react-native';
 import App from './app/App'
-// import App from './redux-immutable/App'
 
 AppRegistry.registerComponent('myRNApp', () => App);
