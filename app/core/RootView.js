e6dea2da0fa2708c2616228a83db7667d0b562ab
张宇 <51410206@qq.com>
2017-09-29 14:13:18 +0800

    添加返回按钮支持

--- app/core/RootView.js
+++ app/core/RootView.js
@@ -1,37 +1,51 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { addNavigationHelpers } from 'react-navigation';
-import { RootNavigator } from './routes';
-import { Screen } from '../components';
+import React, { Component, PropTypes } from 'react'
+import { BackAndroid } from 'react-native'
+import { connect } from 'react-redux'
+import { addNavigationHelpers, NavigationActions } from 'react-navigation'
+import { RootNavigator } from './routes'
+import { Screen } from '../components'
 
 class RootView extends Component {
-    static propTypes = {
-        dispatch: PropTypes.func.isRequired,
-        initialized: PropTypes.bool.isRequired,
-    };
-    
-    componentDidMount(){
-        // SplashScreen.hide()
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    initialized: PropTypes.bool.isRequired,
+  }
+
+  componentDidMount () {
+    BackAndroid.addEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  componentWillUnmount () {
+    BackAndroid.removeEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  onBackPress = () => {
+    const {dispatch, nav} = this.props
+    if (nav.index === 0) {
+      return false
     }
-    
-    render(){
-        if(!this.props.initialized){
-            return <Screen/>
-        }
-        
-        const navigation = addNavigationHelpers({
-            dispatch:this.props.dispatch,
-            state:this.props.nav,
-        });
-        return (
-            <RootNavigator navigation={navigation}/>
-        )
+    dispatch(NavigationActions.back())
+    return true
+  }
+
+  render () {
+    if (!this.props.initialized) {
+      return <Screen/>
     }
+
+    const navigation = addNavigationHelpers({
+      dispatch: this.props.dispatch,
+      state: this.props.nav,
+    })
+    return (
+      <RootNavigator navigation={navigation}/>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-    nav: state.persistent.nav,
-    initialized: state.transient.app.initialized,
-});
+  nav: state.persistent.nav,
+  initialized: state.transient.app.initialized,
+})
 
-export default connect(mapStateToProps)(RootView);
+export default connect(mapStateToProps)(RootView)
